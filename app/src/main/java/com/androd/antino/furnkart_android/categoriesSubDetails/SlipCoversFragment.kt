d7c9ca9cc556d80a4b7f8f58f343f11982a7cc95
Anjali Bindal <anjalibindal628@gmail.com>
2022-02-15 22:46:40 +0530

    categories_sub_details is completed.

--- app/src/main/java/com/androd/antino/furnkart_android/categoriesSubDetails/SlipCoversFragment.kt
+++ app/src/main/java/com/androd/antino/furnkart_android/categoriesSubDetails/SlipCoversFragment.kt
@@ -1,62 +1,67 @@
 package com.androd.antino.furnkart_android.categoriesSubDetails
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.androd.antino.furnkart_android.R
-import com.androd.antino.furnkart_android.categoriesSubDetails.SlipCoversFragmentAdapter
 import com.androd.antino.furnkart_android.databinding.FragmentSlipCoversBinding
-import com.androd.antino.furnkart_android.databinding.FragmentSlipcoversBinding
+import com.androd.antino.furnkart_android.model.SlipCoversModel
 
 class SlipcoversFragment : Fragment() {
-    private lateinit var recyclerView_slipcovers: SlipCoversFragmentAdapter
 
-    private lateinit var binding: FragmentSlipCoversBinding
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerView_slipcover_Adapter: SlipCoversFragmentAdapter
+    private lateinit var dataList:ArrayList<SlipCoversModel>
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_slip_covers, container, false)
 
+        recyclerView=view.findViewById(R.id.rv_slipcovers)
+        dataList=ArrayList()
 
-        // Inflate the layout for this fragment
-        val view= inflater!!.inflate(R.layout.fragment_slipcovers, container, false)
+        initRecyclerView()
+
+        dataList.add(SlipCoversModel(R.drawable.living_1,"$166",1))
+        dataList.add(SlipCoversModel(R.drawable.living_2,"$166",1))
+        dataList.add(SlipCoversModel(R.drawable.living_3,"$166",1))
+        dataList.add(SlipCoversModel(R.drawable.living_4,"$166",1))
+        dataList.add(SlipCoversModel(R.drawable.living_1,"$166",1))
+        dataList.add(SlipCoversModel(R.drawable.living_1,"$166",1))
+        dataList.add(SlipCoversModel(R.drawable.living_2,"$166",1))
+        dataList.add(SlipCoversModel(R.drawable.living_3,"$166",1))
+        dataList.add(SlipCoversModel(R.drawable.living_4,"$166",1))
+        dataList.add(SlipCoversModel(R.drawable.living_1,"$166",1))
+
+        recyclerView_slipcover_Adapter.notifyDataSetChanged()
+
         return view
-
-
-        //initRecyclerView()
-
-
-        /* val dogRepository = DogRepository()
-         dogViewModel = ViewModelProvider(this, viewModelFactory)[DogViewModel::class.java]
-         dogViewModel.getDogPhotos()
-         dogViewModel.dogMutableLiveData.observe(this, Observer {
-
-             for (i in 0 until it.size) {
-                 Log.d("TAG_IMAGES_OF_DOG", "onCreate: " + it.get(i).url)
-
-             }
-             dogAdapter.setData(it as ArrayList<Dog>)
-
-             binding.progressBar.visibility= View.INVISIBLE
-             binding.recyclerView.visibility= View.VISIBLE
-         })
-
-     }
-
-     private fun initRecyclerView() {
-
-         dogAdapter = DogAdapter(this, ArrayList())
-         binding.rvSlipcovers.apply {
-             setHasFixedSize(true)
-             layoutManager = GridLayoutManager(this)
-             adapter = dogAdapter
-*/
     }
 
+
+    private fun initRecyclerView() {
+
+      recyclerView_slipcover_Adapter= context?.let { SlipCoversFragmentAdapter(it,dataList) }!!
+        recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = StaggeredGridLayoutManager(2,LinearLayoutManager.VERTICAL)
+            adapter = recyclerView_slipcover_Adapter
+
+        }
+
+    }
 }
+
+
 
 
 
--- app/src/main/java/com/androd/antino/furnkart_android/categoriesSubDetails/SlipCoversFragmentAdapter.kt
+++ app/src/main/java/com/androd/antino/furnkart_android/categoriesSubDetails/SlipCoversFragmentAdapter.kt
@@ -4,20 +4,24 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.androd.antino.furnkart_android.R
 import com.androd.antino.furnkart_android.model.SlipCoversModel
 
-class SlipCoversFragmentAdapter(var context: Context) :
+class SlipCoversFragmentAdapter(private val context:Context, private var slipList:ArrayList<SlipCoversModel>) :
         RecyclerView.Adapter<SlipCoversFragmentAdapter.SlipCoverViewHolder>()
 {
 
+    class SlipCoverViewHolder(itemView:View):RecyclerView.ViewHolder(itemView)
+    {
+        val iv_living1:ImageView=itemView.findViewById(R.id.iv_living1)
+        val ib_fav:ImageButton=itemView.findViewById(R.id.ib_fav)
+        val tv_rate:TextView=itemView.findViewById(R.id.tv_rate)
+        val view_each_item:View=itemView.findViewById(R.id.view_each_item)
 
-    var dataList = emptyList<SlipCoversModel>()
-
-    suspend fun setDataList(dataList: List<SlipCoversModel>) {
-        this.dataList = dataList
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SlipCoverViewHolder {
@@ -27,20 +31,27 @@ class SlipCoversFragmentAdapter(var context: Context) :
     }
 
     override fun onBindViewHolder(holder: SlipCoverViewHolder, position: Int) {
-        holder.iv_living1
+        holder.iv_living1.setImageResource(slipList.get(position).iv_living1)
+        //holder.ib_fav.setImageResource(slipList.get(position).ib_fav)
+        holder.tv_rate.text=slipList.get(position).tv_rate
+
+        if (position==1)
+        {
+            holder.view_each_item.visibility=View.VISIBLE
+
+        }
+        else{
+            holder.view_each_item.visibility=View.GONE
+        }
     }
 
     override fun getItemCount(): Int {
-        return dataList.size
+        return slipList.size
     }
 
-    class SlipCoverViewHolder(itemView:View):RecyclerView.ViewHolder(itemView)
-    {
-        val iv_living1:ImageView=itemView.findViewById(R.id.iv_living1)
-
-        val ib_
-
+ /*   fun setDataList(dataList: List<SlipCoversModel>) {
+        this.dataList = dataList
     }
-
+*/
 
 }
